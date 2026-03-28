7ce090582a4e5c87efcefd45dec5e66c55a52d81
ing-arriola <ingeniero.arriola@gmail.com>
2026-03-28 15:27:41 -0600

    added message service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from "./messages.repository";
+
+export class MessagesService {
+    messagesRepo: MessagesRepository;
+    constructor() {
+        //services is creating its onw dependencies
+        //DON'T DO THIS IN A REAL APP
+        this.messagesRepo = new MessagesRepository();
+    }
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+}
