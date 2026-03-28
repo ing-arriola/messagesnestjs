55de71ccf37b3f265e892df75c63e042d0db6800
ing-arriola <ingeniero.arriola@gmail.com>
2026-03-28 15:16:35 -0600

    added repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,21 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessagesRepository {
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages[id];
+    }
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages;
+    }
+    async create(message: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        const id = Math.floor(Math.random() * 1000);
+        messages[id] = { id, content: message };
+        await writeFile('messages.json', JSON.stringify(messages));
+    }
+}
